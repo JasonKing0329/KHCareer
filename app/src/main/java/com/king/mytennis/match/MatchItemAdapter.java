7177d8fd69724eb8a2eccdb214beacebc3b7403a
JingYang <jingyang@tcsl.com.cn>
2017-03-06 17:51:59 +0800

    v3.1.1 1. make match and player manage list show more details and images 2. fix issue that player birthday couldn't input normal text

--- app/src/main/java/com/king/mytennis/match/MatchItemAdapter.java
+++ app/src/main/java/com/king/mytennis/match/MatchItemAdapter.java
@@ -6,10 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.king.mytennis.model.ImageFactory;
 import com.king.mytennis.pubdata.bean.MatchNameBean;
+import com.king.mytennis.service.ImageUtil;
 import com.king.mytennis.view.R;
+import com.king.mytennis.view_v_7_0.view.CircleImageView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,12 +60,18 @@ public class MatchItemAdapter extends RecyclerView.Adapter<MatchItemAdapter.Item
         MatchNameBean bean = list.get(position);
         holder.tvIndex.setText(String.valueOf(position + 1));
         holder.tvName.setText(bean.getName());
-        holder.tvWeek.setText(String.valueOf(bean.getMatchBean().getWeek()));
+        holder.tvInfor.setText(bean.getMatchBean().getLevel() + "/" + bean.getMatchBean().getCourt()
+                + " W" + String.valueOf(bean.getMatchBean().getWeek()));
+        holder.tvCountry.setText(bean.getMatchBean().getCountry());
+        holder.tvCity.setText(bean.getMatchBean().getCity());
         holder.check.setVisibility(selectMode ? View.VISIBLE:View.GONE);
         holder.check.setChecked(mCheckMap.get(position));
 
         holder.group.setTag(position);
         holder.group.setOnClickListener(this);
+
+        String filePath = "file://" + ImageFactory.getMatchHeadPath(bean.getName(), bean.getMatchBean().getCourt());
+        ImageUtil.load(filePath, holder.image);
     }
 
     @Override
@@ -102,15 +112,21 @@ public class MatchItemAdapter extends RecyclerView.Adapter<MatchItemAdapter.Item
         ViewGroup group;
         TextView tvIndex;
         TextView tvName;
-        TextView tvWeek;
+        TextView tvCountry;
+        TextView tvCity;
+        TextView tvInfor;
         CheckBox check;
+        ImageView image;
         public ItemHolder(View itemView) {
             super(itemView);
             group = (ViewGroup) itemView.findViewById(R.id.manage_item_group);
             tvIndex = (TextView) itemView.findViewById(R.id.manage_item_index);
             tvName = (TextView) itemView.findViewById(R.id.manage_item_name);
-            tvWeek = (TextView) itemView.findViewById(R.id.manage_item_sequence);
+            tvCountry = (TextView) itemView.findViewById(R.id.manage_item_country);
+            tvCity = (TextView) itemView.findViewById(R.id.manage_item_city);
+            tvInfor = (TextView) itemView.findViewById(R.id.manage_item_infor);
             check = (CheckBox) itemView.findViewById(R.id.manage_item_check);
+            image = (ImageView) itemView.findViewById(R.id.manage_item_image);
         }
     }
 }
--- app/src/main/java/com/king/mytennis/player/PlayerItemAdapter.java
+++ app/src/main/java/com/king/mytennis/player/PlayerItemAdapter.java
@@ -8,8 +8,11 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import com.king.mytennis.model.ImageFactory;
 import com.king.mytennis.pubdata.bean.PlayerBean;
+import com.king.mytennis.service.ImageUtil;
 import com.king.mytennis.view.R;
+import com.king.mytennis.view_v_7_0.view.CircleImageView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +51,7 @@ public class PlayerItemAdapter extends RecyclerView.Adapter<PlayerItemAdapter.It
 
     @Override
     public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ItemHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_match_manage_item, parent, false));
+        return new ItemHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_player_manage_item, parent, false));
     }
 
     @Override
@@ -56,6 +59,8 @@ public class PlayerItemAdapter extends RecyclerView.Adapter<PlayerItemAdapter.It
         PlayerBean bean = list.get(position);
         holder.tvIndex.setText(String.valueOf(position + 1));
         holder.tvName.setText(bean.getNameChn());
+        holder.tvNameEng.setText(bean.getNameEng());
+        holder.tvBirthday.setText(bean.getBirthday());
         holder.tvCountry.setText(bean.getCountry());
         if (selectMode) {
             if (position < PlayerManageActivity.FIXED_PLAYER) {// 不允许删除
@@ -72,6 +77,9 @@ public class PlayerItemAdapter extends RecyclerView.Adapter<PlayerItemAdapter.It
 
         holder.group.setTag(position);
         holder.group.setOnClickListener(this);
+
+        String filePath = "file://" + ImageFactory.getPlayerHeadPath(list.get(position).getNameChn());
+        ImageUtil.load(filePath, holder.image, R.drawable.icon_list);
     }
 
     @Override
@@ -112,15 +120,21 @@ public class PlayerItemAdapter extends RecyclerView.Adapter<PlayerItemAdapter.It
         ViewGroup group;
         TextView tvIndex;
         TextView tvName;
+        TextView tvNameEng;
         TextView tvCountry;
+        TextView tvBirthday;
         CheckBox check;
+        CircleImageView image;
         public ItemHolder(View itemView) {
             super(itemView);
             group = (ViewGroup) itemView.findViewById(R.id.manage_item_group);
             tvIndex = (TextView) itemView.findViewById(R.id.manage_item_index);
             tvName = (TextView) itemView.findViewById(R.id.manage_item_name);
-            tvCountry = (TextView) itemView.findViewById(R.id.manage_item_sequence);
+            tvCountry = (TextView) itemView.findViewById(R.id.manage_item_country);
+            tvNameEng = (TextView) itemView.findViewById(R.id.manage_item_name_eng);
+            tvBirthday = (TextView) itemView.findViewById(R.id.manage_item_birthday);
             check = (CheckBox) itemView.findViewById(R.id.manage_item_check);
+            image = (CircleImageView) itemView.findViewById(R.id.manage_item_image);
         }
     }
 }
